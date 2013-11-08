f4b333e247c4fafd1eb67fd477ec6c81a49da928
Katie Thomas <katiemthom@Katies-MacBook-Pro.local>
2013-11-08 13:52:44 -0800

    added pagination class to model

--- model.py
+++ model.py
@@ -3,7 +3,8 @@ from sqlalchemy import create_engine, ForeignKey, desc
 from sqlalchemy import Column, Integer, String, Date, Text, Float, Boolean, DateTime
 from sqlalchemy.orm import sessionmaker, scoped_session, relationship, backref
 from flask.ext.login import UserMixin
-from flask.ext.sqlalchemy import Pagination
+from math import ceil
+
 import os 
 import config 
 import bcrypt 
@@ -81,6 +82,36 @@ class Notes(Base):
 	link = Column(String(120), nullable = True)
 	created_on = Column(Date)
 
+class Pagination(object):
+
+	def __init__(self, page, per_page, total_count):
+		self.page = page
+		self.per_page = per_page
+		self.total_count = total_count
+
+	@property
+	def pages(self):
+		return int(ceil(self.total_count / float(self.per_page)))
+
+	@property
+	def has_prev(self): 
+		return self.page > 1
+
+	@property
+	def has_next(self):
+		return self.page < self.pages
+
+	def iter_pages(self, left_edge=2, left_current=2,right_current=5,right_edge=2):
+		last = 0
+		for num in xrange(1, self.pages + 1):
+			if num <= left_edge or \
+				(num > self.page - left_current - 1 and \
+				num < sel.page + right_current) or \
+				num > self.pages - right_edge:
+				if last + 1 != num: 
+					yield None
+				yield num
+				last = num 
 ########### FUNCTIONS ###########
 
 def create_db():
