5f71bf981738d3477d65109ca69c0a3965ccd18c
Katie Thomas <katiemthom@Katies-MacBook-Pro.local>
2013-11-12 17:13:19 -0800

    updated requirements

--- static/js/addpost.js
+++ static/js/addpost.js
@@ -1,8 +1,12 @@
-$('#test').click(function() {
-	$('#post-editor').selection('replace', {text: '**'+$('#post-editor').selection()+'**'});
+ $('#test').click(function() {
+	$('#post-editor').selection('replace', {text: '[b]'+$('#post-editor').selection()+'[/b]'});
 });
 
 $('#preview').click(function() {
-	$('#show-preview').html('{% filter markdown %}'+$('#post-editor').val()+'{% endfilter %}');
+	$('#show-preview').text($('#post-editor').val())
+	var unparsed_html = $('#show-preview').html()
+	parsed_html = unparsed_html.replace('[b]', '<b>');
+	parsed_html = parsed_html.replace('[/b]', '</b>')
+	$('#show-preview').html(parsed_html);
 });
 
