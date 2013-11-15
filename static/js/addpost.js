7a726c0bb3326af68621af47b0dc874c1cca346c
Katie Thomas <katiemthom@Katies-MacBook-Pro.local>
2013-11-15 08:04:54 -0800

    starting to get a dialog working for links in add post

--- static/js/addpost.js
+++ static/js/addpost.js
@@ -10,9 +10,9 @@ $('#header').click(function() {
 	$('#post_content').selection('replace', {text: '###'+$('#post_content').selection()});
 });
 
-$('#link').click(function() {
-	$('#post_content').selection('replace', {text: '[replace]('+$('#post_content').selection()+')'});
-});
+// $('#link').click(function() {
+// 	$('#post_content').selection('replace', {text: '[replace]('+$('#post_content').selection()+')'});
+// });
 
 setInterval(function() {
 	$.ajax({
